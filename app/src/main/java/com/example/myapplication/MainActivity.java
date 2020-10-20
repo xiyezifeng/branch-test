1cd493f6ed9baeda917ccb44f03ac5c9aa4dab2b
xiyezifeng <xiyezifeng@163.com>
2020-10-20 09:47:52 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,9 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("MainActivity", "这是debug代码1");
         Log.e("MainActivity", "这是测试从dev合并到debug的代码{dev}");
         Log.e("MainActivity", "这是测试从dev吧需要测试的代码放到debug上{dev}");
         Log.e("MainActivity", "分支合并到主干代码");
+        Log.e("MainActivity", "这是debug代码1");
     }
 }
